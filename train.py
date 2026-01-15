356afaddfa46ca4b98e2b480741ba993444e371f
PattrapornPhaotajai <pattrapornpraw@gmail.com>
2026-01-15 15:06:23 +0700

    ..

--- train.py
+++ train.py
@@ -9,9 +9,12 @@ def main():
     print(f"Training with data from: {data_path}")
     model.train(
         data=data_path,
-        epochs=2,
+        epochs=300,
         imgsz=64,
-        batch=64
+        batch=64,
+        patience=50,
+        device="0",
+
     )
     metrics = model.val()
     print(metrics.results_dict)
